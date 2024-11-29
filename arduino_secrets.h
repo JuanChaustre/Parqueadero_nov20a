e860efdb8d5e9ecc154e6d6d772c523fbb4911c0
JuanChaustre <117322307+JuanChaustre@users.noreply.github.com>
2024-11-29 14:08:14 -0500

    Update arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,3 +1,3 @@
-#define SECRET_DEVICE_KEY "eoUNbuZomSIKSO0v50QAy2fIN"
-#define SECRET_OPTIONAL_PASS "WemosD1R32"
-#define SECRET_SSID "RedmiNote9S"
+#define SECRET_DEVICE_KEY "eoUNbuZomSIKSO0v50QAy2fIN" //Poner clave secreta generada en Arduino Cloud
+#define SECRET_OPTIONAL_PASS "WemosD1R32" //Poner nombre de la Red Wifi donde se debe conectar el sensor
+#define SECRET_SSID "RedmiNote9S" //Contraseña de la red
